68238a4ba7a0b3ceea697da1c16c436fd864a240
dongwook <hsw50108@gmail.com>
2024-06-23 20:11:42 +0900

    IntegrationTest 클래스 추가 / RDBMS test containers 추가

--- src/test/java/me/dongwook/dayonetest/DayonetestApplicationTests.java
+++ src/test/java/me/dongwook/dayonetest/DayonetestApplicationTests.java
@@ -1,13 +1,39 @@
 package me.dongwook.dayonetest;
 
+import jakarta.persistence.EntityManager;
+import me.dongwook.dayonetest.model.StudentScoreFixture;
+import me.dongwook.dayonetest.repository.StudentScoreRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
-class DayonetestApplicationTests {
+class DayonetestApplicationTests extends IntegrationTest {
+
+    @Autowired
+    private StudentScoreRepository studentScoreRepository;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Test
     void contextLoads() {
+
+        var studentScore = StudentScoreFixture.passed();
+        var savedStudentScore = studentScoreRepository.save(studentScore);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        var queryStudentScore = studentScoreRepository.findById(savedStudentScore.getId()).orElseThrow();
+        System.out.println("queryStudentScore.getId() = " + queryStudentScore.getId());
+
+        Assertions.assertEquals(queryStudentScore.getId(), savedStudentScore.getId());
+        Assertions.assertEquals(queryStudentScore.getStudentName(), savedStudentScore.getStudentName());
+        Assertions.assertEquals(queryStudentScore.getMathScore(), savedStudentScore.getMathScore());
+        Assertions.assertEquals(queryStudentScore.getKorScore(), savedStudentScore.getKorScore());
+        Assertions.assertEquals(queryStudentScore.getEnglishScore(), savedStudentScore.getEnglishScore());
+
+
     }
 
 }
